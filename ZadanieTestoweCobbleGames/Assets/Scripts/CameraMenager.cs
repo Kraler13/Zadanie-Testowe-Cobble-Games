b26ebbfe7c4940b1a0b6ab534b29daa5b81a0cc8
Kraler <filip.nowak7@gmail.com>
2024-01-10 14:46:41 +0100

    Zmiany kamery

---
+++ ZadanieTestoweCobbleGames/Assets/Scripts/CameraMenager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenager : MonoBehaviour
+{
+    public Transform target; 
+    public float smoothSpeed = 0.125f; 
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + new Vector3(-74.4f, 66.8f, -88.7f); 
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+
+            transform.rotation = Quaternion.Euler(30f, 40f, 0f);
+
+
+        }
+    }
+}
--- ZadanieTestoweCobbleGames/Assets/Scripts/ChooseAHero.cs
+++ ZadanieTestoweCobbleGames/Assets/Scripts/ChooseAHero.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseAHero : MonoBehaviour
 {
+    [SerializeField] private Button heroButton1;
+    [SerializeField] private Button heroButton2;
+    [SerializeField] private Button heroButton3;
+    //[SerializeField] private List<Button> heroButtons;
+    //[SerializeField] private List<Transform> hero;
+    [SerializeField] protected GameObject heroOne;
+    [SerializeField] protected GameObject heroTwo;
+    [SerializeField] protected GameObject heroThr;
+    [SerializeField] private CameraMenager CameraMenager;
+    [SerializeField] private InputMenager InputMenager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        heroButton1.onClick.AddListener(OnHeroButtonOneClick);
+        heroButton2.onClick.AddListener(OnHeroButtonTwoClick);
+        heroButton3.onClick.AddListener(OnHeroButtonThrClick);
+        //for (int i = 0; i < heroButtons.Count; i++)
+        //{
+        //    int heroIndex = i;
+        //    heroButtons[i].onClick.AddListener(() => CameraMenager.target = hero[heroIndex]);
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnHeroButtonOneClick()
     {
-        
+        Debug.Log("Button One Clicked");
+        CameraMenager.target = heroOne.transform;
+        InputMenager.hero = heroOne.GetComponent<Hero>();
+    }
+    private void OnHeroButtonTwoClick()
+    {
+        Debug.Log("Button Two Clicked");
+        CameraMenager.target = heroTwo.transform;
+        InputMenager.hero = heroTwo.GetComponent<Hero>();
+
+    }
+    private void OnHeroButtonThrClick()
+    {
+        Debug.Log("Button Thr Clicked");
+        CameraMenager.target = heroThr.transform;
+        InputMenager.hero = heroThr.GetComponent<Hero>();
+
     }
 }
